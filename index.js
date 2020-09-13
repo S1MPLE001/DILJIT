0515d8c7d5e3fc798798f2c5ac9d1b80076dda8e
Rajesh Kumar <31917609+S1MPLE001@users.noreply.github.com>
2020-09-13 17:17:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -21,8 +21,8 @@ restService.post("/echo", function(req, res) {
       ? "Enter a numerical number" 
       : 2 * (req.body.queryResult.parameters.number);
   
-   speech = (req.body.queryResult.parameters.number) && speech > 50 ? '<audio src="https://actions.google.com/sounds/v1/cartoon/slide_whistle.ogg">did not get your audio file</audio>':
-   speech ;
+  // speech = (req.body.queryResult.parameters.number) && speech > 50 ? '<audio src="https://actions.google.com/sounds/v1/cartoon/slide_whistle.ogg">did not get your audio file</audio>':
+  // speech ;
   
   var speechResponse = {
     google: {
